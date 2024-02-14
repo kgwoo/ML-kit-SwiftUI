e55d27fd2a7be7604ae4e76e933fdd92e3e29be0
kgwoo <gunoo0126@gmail.com>
2024-02-15 00:36:04 +0900

    Initial Commit

---
+++ ml-kit-swiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ml-kit-swiftUI
+//
+//  Created by GunWoo on 2/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ml-kit-swiftUI/ml_kit_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ml_kit_swiftUIApp.swift
+//  ml-kit-swiftUI
+//
+//  Created by GunWoo on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ml_kit_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
